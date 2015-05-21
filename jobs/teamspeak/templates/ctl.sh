a47b044cb1f654892b49db6a6704239ba06ceb83
Brian Cunnie <cunnie@pivotal.io>
2015-05-21 16:58:45 -0400

    Updates start and packaging script
    [#94754160]
    
    Signed-off-by: Derwei Chan <derwei@pivotal.io>

--- jobs/teamspeak/templates/ctl.sh
+++ jobs/teamspeak/templates/ctl.sh
@@ -2,59 +2,30 @@
 # Copyright (c) 2010 TeamSpeak Systems GmbH
 # All rights reserved
 
-COMMANDLINE_PARAMETERS="${2}" #add any command line parameters you want to pass here
-D1=$(readlink -f "$0")
-BINARYPATH="$(dirname "${D1}")"
-cd "${BINARYPATH}"
-LIBRARYPATH="$(pwd)"
+#COMMANDLINE_PARAMETERS="${2}" #add any command line parameters you want to pass here
+#D1=$(readlink -f "$0")
+#BINARYPATH="$(dirname "${D1}")"
+#cd "${BINARYPATH}"
+#LIBRARYPATH="$(pwd)"
 
+BINARYPATH=/var/vcap/packages/teamspeak-3.0.11.3
+LIBRARYPATH=$BINARYPATH
+BINARYNAME=ts3server_linux_amd64
+TEAMSPEAK_PID=/var/vcap/sys/run/teamspeak/teamspeak.pid
 
-if [ -e ts3server_linux_x86 ]; then
-	if [ -z "`uname | grep Linux`" -o ! -z "`uname -m | grep 64`" ]; then
-                echo "Do you have the right TS3 Server package for your system? You have: `uname` `uname -m`, not Linux i386."
-        fi
-        BINARYNAME="ts3server_linux_x86"
-elif [ -e ts3server_linux_amd64 ]; then
-        if [ -z "`uname | grep Linux`" -o -z "`uname -m | grep 64`" ]; then
-                echo "Do you have the right TS3 Server package for your system? You have: `uname` `uname -m`, not Linux x86_64."
-        fi
-        BINARYNAME="ts3server_linux_amd64"
-elif [ -e ts3server_freebsd_x86 ]; then
-        if [ -z "`uname | grep FreeBSD`" -o ! -z "`uname -m | grep 64`" ]; then
-                echo "Do you have the right TS3 Server package for your system? You have: `uname` `uname -m`, not FreeBSD i386."
-        fi
-        BINARYNAME="ts3server_freebsd_x86"
-elif [ -e ts3server_freebsd_amd64 ]; then
-        if [ -z "`uname | grep FreeBSD`" -o -z "`uname -m | grep 64`" ]; then
-                echo "Do you have the right TS3 Server package for your system? You have: `uname` `uname -m`, not FreeBSD amd64."
-        fi
-        BINARYNAME="ts3server_freebsd_amd64"
-else
-	echo "Could not locate binary file, aborting"
-	exit 5
-fi
+cd $BINARYPATH
 
 case "$1" in
 	start)
-		if [ -e ts3server.pid ]; then
-			if ( kill -0 $(cat ts3server.pid) 2> /dev/null ); then
+		if [ -e $TEAMSPEAK_PID ]; then
+			if ( kill -0 $(cat $TEAMSPEAK_PID) 2> /dev/null ); then
 				echo "The server is already running, try restart or stop"
 				exit 1
 			else
-				echo "ts3server.pid found, but no server running. Possibly your previously started server crashed"
+				echo "$TEAMSPEAK_PID found, but no server running. Possibly your previously started server crashed"
 				echo "Please view the logfile for details."
-				rm ts3server.pid
+				rm $TEAMSPEAK_PID
 			fi
-		fi
-		if [ "${UID}" = "0" ]; then
-			echo WARNING ! For security reasons we advise: DO NOT RUN THE SERVER AS ROOT
-			c=1
-			while [ "$c" -le 10 ]; do
-				echo -n "!"
-				sleep 1
-				c=$(($c+1))
-			done
-			echo "!"
 		fi
 		echo "Starting the TeamSpeak 3 server"
 		if [ -e "$BINARYNAME" ]; then
@@ -70,7 +41,7 @@ case "$1" in
 				if [ "$?" -ne "0" ]; then
 					echo "TeamSpeak 3 server could not start"
 				else
-					echo $PID > ts3server.pid
+					echo $PID > $TEAMSPEAK_PID
 					echo "TeamSpeak 3 server started, for details please view the log file"
 				fi
 			else
@@ -82,12 +53,12 @@ case "$1" in
 		fi
 	;;
 	stop)
-		if [ -e ts3server.pid ]; then
+		if [ -e $TEAMSPEAK_PID ]; then
 			echo -n "Stopping the TeamSpeak 3 server"
-			if ( kill -TERM $(cat ts3server.pid) 2> /dev/null ); then
+			if ( kill -TERM $(cat $TEAMSPEAK_PID) 2> /dev/null ); then
 				c=1
 				while [ "$c" -le 300 ]; do
-					if ( kill -0 $(cat ts3server.pid) 2> /dev/null ); then
+					if ( kill -0 $(cat $TEAMSPEAK_PID) 2> /dev/null ); then
 						echo -n "."
 						sleep 1
 					else
@@ -96,15 +67,15 @@ case "$1" in
 					c=$(($c+1))
 				done
 			fi
-			if ( kill -0 $(cat ts3server.pid) 2> /dev/null ); then
+			if ( kill -0 $(cat $TEAMSPEAK_PID) 2> /dev/null ); then
 				echo "Server is not shutting down cleanly - killing"
-				kill -KILL $(cat ts3server.pid)
+				kill -KILL $(cat $TEAMSPEAK_PID)
 			else
 				echo "done"
 			fi
-			rm ts3server.pid
+			rm $TEAMSPEAK_PID
 		else
-			echo "No server running (ts3server.pid is missing)"
+			echo "No server running ($TEAMSPEAK_PID is missing)"
 			exit 7
 		fi
 	;;
@@ -112,14 +83,14 @@ case "$1" in
 		$0 stop && $0 start ${COMMANDLINE_PARAMETERS} || exit 1
 	;;
 	status)
-		if [ -e ts3server.pid ]; then
-			if ( kill -0 $(cat ts3server.pid) 2> /dev/null ); then
+		if [ -e $TEAMSPEAK_PID ]; then
+			if ( kill -0 $(cat $TEAMSPEAK_PID) 2> /dev/null ); then
 				echo "Server is running"
 			else
 				echo "Server seems to have died"
 			fi
 		else
-			echo "No server running (ts3server.pid is missing)"
+			echo "No server running ($TEAMSPEAK_PID is missing)"
 		fi
 	;;
 	*)
